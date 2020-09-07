9c6f0d4043f9a20b5084d944d92175422b52b6b8
amberlin721 <70179572+amberlin721@users.noreply.github.com>
2020-09-07 12:52:31 -0400

    Add files via upload

---
+++ Workbook Assignment 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+Location = "C:/Users/xlin8/Downloads/all_040_in_01.P1.csv"
+df = pd.read_csv(Location, header=None)
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
