1119428b50522ebd7f6faaddfb091e04d3946cfc
amberlin721 <70179572+amberlin721@users.noreply.github.com>
2020-09-07 12:24:37 -0400

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+Location = "C:/Users/xlin8/OneDrive/Desktop/datasets/smallgradesh.csv"
+df = pd.read_csv(Location, header=None)
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
