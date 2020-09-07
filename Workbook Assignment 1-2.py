81199b16162464c2f231d9a3b6c29a0055fdb35a
amberlin721 <70179572+amberlin721@users.noreply.github.com>
2020-09-07 13:03:46 -0400

    Add files via upload

---
+++ Workbook Assignment 1-2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+Location = "C:/Users/xlin8/Downloads/AGN01.xls"
+df = pd.read_excel(Location)
+
+
+# In[7]:
+
+
+df. head()
+
+
+# In[ ]:
+
+
+
+
