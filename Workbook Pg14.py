e37eeb21720eff4452cef4023f306888197d693b
amberlin721 <70179572+amberlin721@users.noreply.github.com>
2020-09-17 23:16:26 -0400

    Add files via upload

---
+++ Workbook Pg14.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+# Connect to sqlite db
+db_file = r'C:/Users/xlin8/OneDrive/Desktop/ISM 4402/datasets/salesdata.db'
+engine = create_engine(r"sqlite:///{}"
+    .format(db_file))
+sql = "select name from sqlite_master where type = 'table' "
+sales_data_df = pd.read_sql(sql, engine)
+sales_data_df
+
+
+# In[9]:
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+# Connect to sqlite db
+db_file = r'C:/Users/xlin8/OneDrive/Desktop/ISM 4402/datasets/salesdata.db'
+engine = create_engine(r"sqlite:///{}"
+    .format(db_file))
+sql = 'select * from scores'
+sales_data_df = pd.read_sql(sql, engine)
+sales_data_df
+
+
+# In[ ]:
+
+
+
+
